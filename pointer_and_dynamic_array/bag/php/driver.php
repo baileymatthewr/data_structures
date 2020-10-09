10cd2aae440d4e4e1ca62aebb6d2dd35998e2033
Raymond Chi <raymond.cx30@gmail.com>
2020-10-09 11:28:08 -0600

    move echo line inside do-while loop

--- pointer_and_dynamic_array/bag/php/driver.php
+++ pointer_and_dynamic_array/bag/php/driver.php
@@ -9,8 +9,8 @@ $mean_value = 0;
 echo "This program will compute the average of some numbers. The\n";
 echo "numbers will be stored in an array of doubles that I allocate.\n";
 echo "How many doubles should I allocate?\n";
-echo "Please type a positive integer answer:\n";
 do {
+    echo "Please type a positive integer answer:\n";
     fscanf(STDIN, "%d\n", $array_size);
 } while(!is_numeric($array_size));
 $numbers = new bag($array_size);
