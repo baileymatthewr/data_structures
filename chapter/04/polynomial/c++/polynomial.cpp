1183140c8cd9f8d2238a1991deb36689c1eb2949
Matt Bailey <bailey.matthewr@gmail.com>
2021-01-15 08:52:14 -0700

    write out the methods

--- chapter/04/polynomial/c++/polynomial.cpp
+++ chapter/04/polynomial/c++/polynomial.cpp
@@ -1,21 +1,89 @@
 #include <algorithm>
 #include <iostream>
 #include <cassert>
-#include "mystring.h"
+#include <math.h>
+#include "polynomial.h"
+
 
 using namespace std;
 
-namespace mystring_h {
+namespace polynomial_h {
 
-    string::string(const char str[]){
-        current_length = strlen(str);
-        allocated = current_length + 1;
-        characters = new char[allocated];
-        strcpy(characters, str);
+    Polynomial::Polynomial(){
+        // creates a polynomial with all zero coefficients
+        _degree = 0;
+        _coefficients = new double[_degree + 1];
+        _coefficients[0] = 0.0;
     }
 
-    bool operator ==(const string& s1, const string& s2){
-        return (strcmp(s1.characters, s2.characters) == 0);
+    Polynomial::Polynomial(double a0){
+        // creates a polynomial with the specified parameter as the coefficient
+        // of the x^0 term
+        _degree = 0;
+        _coefficients = new double[_degree + 1];
+        _coefficients[0] = a0;
     }
 
+    Polynomial::Polynomial(const Polynomial& source){
+        _degree = source.degree();
+        size_t count = _degree + 1;
+        _coefficients = new double[count];
+        for(size_t i = 0; i < count; ++i)
+            _coefficients[i] = source.coefficient(i);
+    }
+
+    void Polynomial::add_to_coef(double amount, unsigned int k){
+        _coefficients[k] += amount;
+    }
+
+    void Polynomial::assign_coef(double new_coefficient, unsigned int k){
+        _coefficients[k] = new_coefficient;
+    }
+
+    void Polynomial::clear(){
+        // sets all coefficients to 0
+        unsigned int count = _degree + 1;
+        for(size_t i = 0; i < count; ++i)
+            _coefficients[i] = 0;
+        _degree = 0;
+    }
+
+    void Polynomial::reserve(size_t number){
+        if(number > _degree){
+            unsigned int count = _degree + 1;
+            double *old_coefficients = _coefficients;
+            _coefficients = new double[number];
+            for(size_t i = 0; i < count; ++i)
+                _coefficients[i] = old_coefficients[i];
+            delete old_coefficients;
+        }
+    }
+
+    double Polynomial::eval(double x) const {
+        double output = 0.00;
+        unsigned int count = _degree + 1;
+        for(size_t i = 0; i < count; ++i)
+            output += _coefficients[i] * pow(x, i);
+        return output;
+    }
+
+    double Polynomial::coefficient(unsigned int k) const {
+        return *(_coefficients + k);
+    }
+
+    unsigned int Polynomial::degree() const {
+        return _degree;
+    }
+
+    unsigned int Polynomial::next_term(unsigned int k) const {
+        unsigned int output = UINT_MAX;
+        unsigned int count = _degree + 1;
+        for(size_t i = k; i < count; ++i){
+            if(_coefficients[i] != 0.0){
+                output = i;
+                break;
+            }
+        }
+        return output;
+    }
 }
--- chapter/04/polynomial/c++/polynomial.h
+++ chapter/04/polynomial/c++/polynomial.h
@@ -1,36 +1,74 @@
 #ifndef POLYNOMIAL_H
 #define POLYNOMIAL_H
 #include <cstdlib>
+#include <stdexcept>
 
 namespace polynomial_h {
     class Polynomial {
+    private:
+        double *_coefficients;
+        unsigned int _degree;
     public:
         Polynomial();
         Polynomial(double a0);
         Polynomial(const Polynomial& source);
-        Polynomial(double);
-        ~Polynomial(){ delete coefficients; }
+        ~Polynomial(){ delete _coefficients; }
 
         void add_to_coef(double amount, unsigned int k);
         void assign_coef(double new_coefficient, unsigned int k);
         void clear();
         void reserve(size_t number);
         double eval(double x) const;
-        double operator()(double x) const;
+        double operator()(double x) const {
+            return this->eval(x);
+        }
 
-        Polynomial& operator =(const Polynomial& source);
-        Polynomial& operator =(double a0);
-        Polynomial operator+(const Polynomial& p);
-        Polynomial operator-(const Polynomial& p);
-        Polynomial operator*(const Polynomial& p);
+        Polynomial& operator=(const Polynomial& source){
+            if(&source == this)
+                throw std::runtime_error(std::string("Assignment Failure"));
+            return *this;
+        }
+
+        Polynomial& operator=(double a0){
+            this->clear();
+            _coefficients[0] = a0;
+            return *this;
+        }
+
+        Polynomial operator+(const Polynomial& p) const {
+            if(_degree > p.degree()){
+                Polynomial q = new Polynomial(this);
+                for(size_t i = 0; i <= q.degree(); ++i){
+                    if(i <= p.degree())
+                        q.add_to_coef(p.coefficient(i), i);
+                }
+                return q;
+            } else {
+                Polynomial q = new Polynomial(p);
+                for(size_t i = 0; i <= q.degree(); ++i){
+                    if(i <= _degree)
+                        q.add_to_coef(_coefficient[i], i);
+                }
+                return q;
+            }
+        }
+
+        Polynomial operator-(const Polynomial& p) const {
+            Polynomial q = new Polynomial(this);
+            for(size_t i = 0; i <= q.degree(); ++i){
+                if(i <= p.degree())
+                    q.add_to_coef((1 - p.coefficient(i)), i);
+            }
+            return q;
+        }
+
+        Polynomial operator*(const Polynomial& p) const {
+            //FOIL - FIRST, OUTSIDE, INSIDE, LAST
+        }
 
         double coefficient(unsigned int k) const;
         unsigned int degree() const;
         unsigned int next_term(unsigned int k) const;
-
-    private:
-        double *coefficients;
-        unsigned int degree;
     };
 }
 #endif
